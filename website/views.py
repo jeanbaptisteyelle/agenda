5f5ea6eb5cfb3609a68f7180e514bd94de5c3537
jeanbaptisteyelle <yellejeanbaptiste@gmail.com>
2020-09-11 12:22:04 +0000

    transformation requete latest par order_by

--- website/views.py
+++ website/views.py
@@ -35,8 +35,8 @@ def newletter(request):
         
 
 def index(request):      
-    #siteinfo = models.Siteinfo.objects.filter(status=True).latest('date_update')
-    about = models.About.objects.filter(status=True).latest('date_update')
+    siteinfo = models.Siteinfo.objects.get(status=True)
+    about = models.About.objects.filter(status=True).order_by('-date_update')
     lieu = blog_models.Lieu.objects.filter(status=True)
     event = blog_models.Event.objects.filter(status=True)
     nky = event.filter(lieu__ville__icontains="new york")
@@ -46,7 +46,7 @@ def index(request):
 
     datas = {
        
-        #"siteinfo":siteinfo,
+        "siteinfo":siteinfo,
         "about":about,
         "lieu":lieu,
         "reseau":reseau,
